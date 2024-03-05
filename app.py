0df6865cd83761ef0457f1ed5b52f8b3bfc75bea
anaharri <anaharringtonb@gmail.com>
2024-03-05 20:27:54 -0300

    serve static files from static/

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from flask import Flask, render_template, request
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(
+    __name__, template_folder="templates", static_url_path="", static_folder="static"
+)
 
 
 def get_colors(query):
